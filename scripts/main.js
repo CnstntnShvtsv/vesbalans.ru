2b4698631ba67304e3bdf1aeac1d562b13144f94
Constantine Shvetsov <cnstntn@mail.ru>
2020-10-17 21:42:26 +0300

    lp-02 is ready

--- scripts/main.js
+++ scripts/main.js
@@ -24,3 +24,26 @@ $('#back-to-top').click(function () {
     }, 500);
     return false;
 });
+
+// Слайдер
+$(document).ready(function () {
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        infinite: true,
+        centerMode: true,
+        variableWidth: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: false,
+    });
+
+    $('.btn-prev').on('click', function () {
+        $('.slider').slick('slickPrev');
+    });
+
+    $('.btn-next').on('click', function () {
+        $('.slider').slick('slickNext');
+    });
+});
