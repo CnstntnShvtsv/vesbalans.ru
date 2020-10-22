d1706e00bf8aad7a7581232f100a129280008ec4
Constantine Shvetsov <cnstntn@mail.ru>
2020-10-23 00:17:40 +0300

    overlay fixes and other

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-// Плавный прокрутка
+// Плавная прокрутка:
 var $page = $('html, body');
 $('a[href*="#"]').click(function () {
     $page.animate({
@@ -25,25 +25,49 @@ $('#back-to-top').click(function () {
     return false;
 });
 
-// Слайдер
-$(document).ready(function () {
-    $('.slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        infinite: true,
-        centerMode: true,
-        variableWidth: true,
-        autoplaySpeed: 5000,
-        arrows: false,
-        dots: false,
-    });
+// Модальные окна:
+// открыть по кнопке:
+$('#btn_modal-01').click(function () {
+	$('#modal-01').fadeIn();
+    $('#modal-01').addClass('disabled');
+    $('#frame-01').attr('src', 'https://www.youtube.com/embed/16DTs9qcvys');
+	$('body').css('overflow', 'hidden');
+});
 
-    $('.btn-prev').on('click', function () {
-        $('.slider').slick('slickPrev');
-    });
+$('#btn_modal-02').click(function () {
+	$('#modal-02').fadeIn();
+	$('#modal-02').addClass('disabled');
+    $('#frame-02').attr('src', 'https://www.youtube.com/embed/EjTo9AMxr5A');
+	$('body').css('overflow', 'hidden');
+});
 
-    $('.btn-next').on('click', function () {
-        $('.slider').slick('slickNext');
-    });
+$('#btn_modal-03').click(function () {
+	$('#modal-03').fadeIn();
+	$('#modal-03').addClass('disabled');
+    $('#frame-03').attr('src', 'https://www.youtube.com/embed/RbgCuf5Tnis');
+	$('body').css('overflow', 'hidden');
+});
+
+$('#btn_modal-04').click(function () {
+	$('#modal-04').fadeIn();
+	$('#modal-04').addClass('disabled');
+    $('#frame-04').attr('src', 'https://www.youtube.com/embed/SZVMCRN6Rqw');
+	$('body').css('overflow', 'hidden');
+});
+
+// закрыть на крестик:
+$('.btn_close').click(function () {
+    $('.modal-wrapper').fadeOut();
+    $('#frame-01, #frame-02, #frame-03, #frame-04').attr('src', '');
+	$('body').css('overflow', 'auto');
+});
+
+// закрыть по клику вне окна:
+$(document).mouseup(function (e) {
+	var popup = $('.modal');
+	if (e.target != popup[0] && popup.has(e.target).length === 0) {
+        $('.modal-wrapper').fadeOut();
+        $('#frame-01, #frame-02, #frame-03, #frame-04').attr('src', '');
+		$('body').css('overflow', 'auto');
+	}
 });
